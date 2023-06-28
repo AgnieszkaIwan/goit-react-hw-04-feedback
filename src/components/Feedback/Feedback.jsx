b2c9f874da36b1df21d0a7800a754c76c54fe7b9
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-28 19:26:36 +0200

    .

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -60,7 +60,7 @@ export const Feedback = () => {
 
   const feedbackOptions = Object.keys(state);
   const totalFeedback = countTotalFeedback();
-  const positivePercentage = countPositiveFeedbackPercentage();
+  // const positivePercentage = countPositiveFeedbackPercentage();
 
   return (
     <div className={styles.container}>
